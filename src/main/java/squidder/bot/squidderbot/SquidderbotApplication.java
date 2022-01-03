32b2f28727510de383fa4ba8d4363ffd1d003605
c2040622 <TolandSO@Cardiff.ac.uk>
2022-01-03 23:48:08 +0000

    Added discord bot login via system variable

--- src/main/java/squidder/bot/squidderbot/SquidderbotApplication.java
+++ src/main/java/squidder/bot/squidderbot/SquidderbotApplication.java
@@ -1,13 +1,22 @@
 package squidder.bot.squidderbot;
 
+import discord4j.core.GatewayDiscordClient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import discord4j.core.DiscordClient;
+import reactor.core.publisher.Mono;
 
 @SpringBootApplication
 public class SquidderbotApplication {
 
     public static void main(String[] args) {
+        // Spring Boot Init
         SpringApplication.run(SquidderbotApplication.class, args);
+
+        // Discord Bot Login
+        DiscordClient client = DiscordClient.create(System.getenv("squidder-bot-token"));
+        Mono<Void> login = client.withGateway((GatewayDiscordClient gateway) -> Mono.empty());
+        login.block();
     }
 
 }
