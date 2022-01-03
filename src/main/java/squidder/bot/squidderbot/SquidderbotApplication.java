1eb303ab886b065ad06627917246feb0ac63d50c
c2040622 <TolandSO@Cardiff.ac.uk>
2022-01-03 22:51:05 +0000

    Initial Spring boot files with Discord4J dependency

---
+++ src/main/java/squidder/bot/squidderbot/SquidderbotApplication.java
@@ -0,0 +1,13 @@
+package squidder.bot.squidderbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SquidderbotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SquidderbotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/squidder/bot/squidderbot/SquidderbotApplicationTests.java
@@ -0,0 +1,13 @@
+package squidder.bot.squidderbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SquidderbotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
