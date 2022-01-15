2dcfa4fc6dcb90430e124a2ebf640303d499d898
Samuel Toland <TolandSO@Cardiff.ac.uk>
2022-01-15 02:52:06 +0000

    Added print for logged in bot.

--- src/main/java/squidder/bot/squidderbot/SquidderbotApplication.java
+++ src/main/java/squidder/bot/squidderbot/SquidderbotApplication.java
@@ -1,6 +1,8 @@
 package squidder.bot.squidderbot;
 
 import discord4j.core.GatewayDiscordClient;
+import discord4j.core.event.domain.lifecycle.ReadyEvent;
+import discord4j.core.object.entity.User;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import discord4j.core.DiscordClient;
@@ -15,7 +17,14 @@ public class SquidderbotApplication {
 
         // Discord Bot Login
         DiscordClient client = DiscordClient.create(System.getenv("squidder-bot-token"));
-        Mono<Void> login = client.withGateway((GatewayDiscordClient gateway) -> Mono.empty());
+        Mono<Void> login = client.withGateway((GatewayDiscordClient gateway) ->
+            gateway.on(ReadyEvent.class, event ->
+                Mono.fromRunnable(() -> {
+                    final User self = event.getSelf();
+                    System.out.printf("Logged in as %s#%s%n", self.getUsername(), self.getDiscriminator());
+                })
+            )
+        );
         login.block();
     }
 
