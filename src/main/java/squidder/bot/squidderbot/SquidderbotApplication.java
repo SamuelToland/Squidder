285258d9f77fcf33961ccb469452a95a11d8c5e5
Samuel Toland <TolandSO@Cardiff.ac.uk>
2022-01-16 04:48:05 +0000

    Removed initial code in the main class

--- src/main/java/squidder/bot/squidderbot/SquidderbotApplication.java
+++ src/main/java/squidder/bot/squidderbot/SquidderbotApplication.java
@@ -1,5 +1,6 @@
 package squidder.bot.squidderbot;
 
+import discord4j.core.DiscordClientBuilder;
 import discord4j.core.GatewayDiscordClient;
 import discord4j.core.event.domain.lifecycle.ReadyEvent;
 import discord4j.core.object.entity.User;
@@ -12,20 +13,7 @@ import reactor.core.publisher.Mono;
 public class SquidderbotApplication {
 
     public static void main(String[] args) {
-        // Spring Boot Init
         SpringApplication.run(SquidderbotApplication.class, args);
-
-        // Discord Bot Login
-        DiscordClient client = DiscordClient.create(System.getenv("squidder-bot-token"));
-        Mono<Void> login = client.withGateway((GatewayDiscordClient gateway) ->
-            gateway.on(ReadyEvent.class, event ->
-                Mono.fromRunnable(() -> {
-                    final User self = event.getSelf();
-                    System.out.printf("Logged in as %s#%s%n", self.getUsername(), self.getDiscriminator());
-                })
-            )
-        );
-        login.block();
     }
 
 }
